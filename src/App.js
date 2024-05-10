c3776719011c5ceb02989c87423e636205d1a255
quickstepp <github.pjwjs@slmail.me>
2024-05-10 13:37:42 +0200

    insert Image

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
+import Image from './components/Image';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<div className='recipe-container'>
+					<Image />
+				</div>
+			</header>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Image.js
@@ -0,0 +1,8 @@
+export default function Image() {
+    return (
+        <img
+            alt="Omlette"
+            className="recipe-image" 
+            src="/assets/images/image-omelette.jpeg" />
+    )
+}
